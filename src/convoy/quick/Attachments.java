114cb7a04208c78d3a3e25c2aaf3507ef71dc764
Mike Moye <mtm5313@psu.edu>
2014-01-29 22:30:47 -0500

    made changes to attachments to test commit and push

--- src/convoy/quick/Attachments.java
+++ src/convoy/quick/Attachments.java
@@ -2,34 +2,89 @@ package convoy.quick;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.List;
+
+//import java.util.List; //Dont need this
 
 public class Attachments {
-    private List<File> attachments;
+    
+    private ArrayList<File> attachments; 
+    //private List<File> attachments;
     
     /**
      * This constructor initializes attachments 
      */
     public Attachments() {
-        attachments = new ArrayList<File>();
+        
+        attachments = new ArrayList<>();
+        //attachments = new ArrayList<File>();
+        
     }
     
-    public void addAttachment(File f) {
-        attachments.add(f);        
+    /**
+     * Method addAttachment(File file) adds attachment
+     * @param file file to be attached
+     */
+    
+    public void addAttachment(File file) {
+        
+        attachments.add(file);
+        
     }
+    
+    /**
+     * Method removeAttachment(File file)
+     * @param file file to be removed
+     */
 	
-    public void removeAttachment(File f) {
-        int i = attachments.indexOf(f);
+    public void removeAttachment(File file) {
+        
+        int i = attachments.indexOf(file);
+        
         if (i >= 0) {
+            
             attachments.remove(i);
+            
         }
+        
+    }
+    /**
+     * Method removeAllAttachments() re-instantiates attachments
+     */
+    
+    public void removeAllAttachments() {
+        
+        attachments = new ArrayList<>();
+        
     }
     
-    public File getAttachment(File f) {
-        int i = attachments.indexOf(f);
+    /**
+     * Method getSingleAttachment(File file) gets a single file
+     * @param file file to be retrieved
+     * @return attachments.get(i) returns selected file or returns null
+     */
+    public File getSingleAttachment(File file) {
+        
+        int i = attachments.indexOf(file);
+        
         if (i >= 0) {
+            
             return attachments.get(i);
+            
         }
+        
         return null;
+        
     }
+    
+    /**
+     * Method getAllAttachments() gets all attachments
+     * @return attachments returns the arrayList of attachments
+     */
+    
+    public ArrayList<File> getAllAttachments() {
+               
+        return attachments;
+        
+    }
+    
 }
