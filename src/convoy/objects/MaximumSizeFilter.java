ab2acdb08e171cbf628cd89a0b342cab328f33e8
Mike Moye <mtm5313@psu.edu>
2014-03-04 13:47:36 -0500

    added documentation to all the files that I created

--- src/convoy/objects/MaximumSizeFilter.java
+++ src/convoy/objects/MaximumSizeFilter.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package convoy.objects;
 
@@ -13,8 +8,13 @@ import javax.swing.text.DocumentFilter;
 import javax.swing.text.Element;
 
 /**
+ * @author Mike Moye <mtm5313@psu.edu>
+ * @version 1.0
+ * @since 2014-02-27
  *
- * @author dizoo548
+ * <p>
+ * This class is used to limit the maximum size of the text areas
+ * </p>
  */
 public class MaximumSizeFilter extends DocumentFilter {  
        
@@ -22,7 +22,7 @@ public class MaximumSizeFilter extends DocumentFilter {
     private final int maxCols;  
    
     /** 
-     * Creates a new <code>MaximumSizeFilter</code> with a given row/column limit. 
+     * Creates a new MaximumSizeFilter with a given row/column limit. 
      * This Filter limits the associated JTextComponent to this number of rows and columns. 
      * @param maxRows the maximum number of rows allowed 
      * @param maxCols the maximum number of columns allowed in any line 
@@ -32,29 +32,37 @@ public class MaximumSizeFilter extends DocumentFilter {
         this.maxCols = maxCols;  
     }  
    
-    /** 
-     * {@inheritDoc} 
-     */  
-      
+    /**
+     * 
+     * @param fb
+     * @param offset
+     * @param str
+     * @param attr
+     * @throws BadLocationException 
+     */
+    @Override
     public void insertString (DocumentFilter.FilterBypass fb, int offset, String str, AttributeSet attr)  
             throws BadLocationException {  
         replace(fb, offset, 0, str, attr);  
     }  
    
-    /** 
-     * {@inheritDoc} 
-     */  
-      
+    /**
+     * 
+     * @param fb
+     * @param offset
+     * @param length
+     * @param str
+     * @param attrs
+     * @throws BadLocationException 
+     */      
+    @Override
     public void replace (DocumentFilter.FilterBypass fb, int offset, int length, String str, AttributeSet attrs)  
             throws BadLocationException {  
-        Document doc = fb.getDocument();  
-   
+        Document doc = fb.getDocument();
         // Apply the insert/replace first, necessary corrections will be made below  
-        fb.replace(offset, length, str, attrs);  
-   
+        fb.replace(offset, length, str, attrs); 
         // Get the root element, meaning the main text element containing the 'line' elements  
-        Element root = doc.getDefaultRootElement();  
-   
+        Element root = doc.getDefaultRootElement();
         // Task 1: Find lines longer than maxCols  
         for (int index=0; index<root.getElementCount(); index++) {  
             Element e = root.getElement(index);  
@@ -63,14 +71,12 @@ public class MaximumSizeFilter extends DocumentFilter {
             if (lineLength > this.maxCols) {  
                 fb.remove(e.getStartOffset()+this.maxCols, lineLength-this.maxCols);  
             }  
-        }  
-   
+        }     
         // Task 2: Clip everything after the last permissible row  
         if (root.getElementCount() > this.maxRows) {  
             int offsetInvalidRow = root.getElement(this.maxRows).getStartOffset();  
             // remove the \n of the previous row too  
             fb.remove(offsetInvalidRow - 1, root.getEndOffset() - offsetInvalidRow);  
-        }  
-   
+        }
     }  
 }      
