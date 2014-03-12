fdb78807f48e88fe4e14190234b70fcc7bf6d2ff
oakes121 <oakes121@gmail.com>
2014-03-12 16:11:04 -0400

    Started to create functionality to populate MainWindows

--- src/convoy/gui/VehicleGrid.java
+++ src/convoy/gui/VehicleGrid.java
@@ -68,9 +68,9 @@ public class VehicleGrid extends javax.swing.JPanel implements ActionListener, K
         setLayout(new GridBagLayout()); 
         createPlaceHolderPanelGrid();     
         
+        panelHolder.get(0).removeAll();
         panelHolder.get(0).add(addNewVehiclePanel1);
-        revalidate();
-        repaint();
+        reDraw();
         
     }
     
@@ -96,19 +96,18 @@ public class VehicleGrid extends javax.swing.JPanel implements ActionListener, K
         if (pos1 == -1) 
             clearBorders();
         
+        
         if (trueCount < 2) {            
             if (vehiclePanelClickedFlags.get(counter).getTruthValue() == false) {
                 vehiclePanelClickedFlags.get(counter).setTruthValue(true);
                 addBorder(counter, 1);  
                 trueCount++;
                 swapPanel();
-                //JOptionPane.showMessageDialog(this, "one "+ trueCount, "count", JOptionPane.WARNING_MESSAGE);
             }
             else if (vehiclePanelClickedFlags.get(counter).getTruthValue() == true) {
                 vehiclePanelClickedFlags.get(counter).setTruthValue(false);
                 --trueCount;
                 pos1 = pos2 = -1;
-                //JOptionPane.showMessageDialog(this, "two "+ trueCount, "count", JOptionPane.WARNING_MESSAGE);
                 addBorder(counter, 0);
             }
         }  else if (trueCount >= 2) {
@@ -116,7 +115,6 @@ public class VehicleGrid extends javax.swing.JPanel implements ActionListener, K
                 vehiclePanelClickedFlags.get(counter).setTruthValue(false);
                 --trueCount;
                 pos1 = pos2 = -1;
-                //JOptionPane.showMessageDialog(this, "three "+ trueCount, "count", JOptionPane.WARNING_MESSAGE);
                 addBorder(counter, 0);
             }            
         }       
@@ -162,34 +160,34 @@ public class VehicleGrid extends javax.swing.JPanel implements ActionListener, K
                 panelHolder.get(m).setBackground((new java.awt.Color(255, 255, 255)));
                 add(panelHolder.get(m), gbc);
         }
+        
     }
     
     /**
-     * addVehiclePanel(int row, int column) adds a vehiclePanel to the next available blank panel
+     * addVehiclePanel() adds a vehiclePanel to the next available blank panel
      *  in the placeHolder array panel Grid
-     * @param row specifies which row in the placeHolder grid to add a vehiclePanel to 
-     * @param column specifies which column in the placeHolder grid to add a vehiclePanel to 
+     * @param addVehiclePanel 
      */
     private void addVehiclePanel(VehiclePanel vp) {
-        if (vehicleCount < 16) {
-            panelHolder.get(vehicleCount).remove(addNewVehiclePanel1);
+        if (vehicleCount < 16) {            
+            
+            vp.addMouseListener(this);
+            vp.getDeleteButton().addActionListener(this);
+            panelHolder.get(vehicleCount).removeAll();
             
             vehiclePanelArray.add(vp);
-            vehiclePanelArray.get(vehicleCount).addMouseListener(this);
-            vehiclePanelArray.get(vehicleCount).getDeleteButton().addActionListener(this);
+            
             panelHolder.get(vehicleCount).add(vehiclePanelArray.get(vehicleCount));
+            
             ++vehicleCount;
             
-            //vehiclePanelMouseListenerUpdate();
-            
-            
-            if (vehicleCount < expNumberOfVehicles ) {
-                panelHolder.get(vehicleCount).add(addNewVehiclePanel1);
-            }
-            else 
-                repaint();
-                revalidate(); 
-        }        
+            panelHolder.get(vehicleCount).add(addNewVehiclePanel1);
+        }
+        else
+            //JOptionPane.showMessageDialog(this, "You cannot add anymore"  , " vehicle Count", JOptionPane.WARNING_MESSAGE);
+        
+        revalidate();
+        repaint();
     }    
 
     /**
@@ -219,7 +217,6 @@ public class VehicleGrid extends javax.swing.JPanel implements ActionListener, K
         }
         
         if ((pos1 != -1) && (pos2 != -1) && (pos1 != pos2)) {      
-            //JOptionPane.showMessageDialog(this, ""+ pos1 + "  " + pos2, "pos1", JOptionPane.WARNING_MESSAGE);
             
             vehiclePanelArray.get(pos1).setBorder(BorderFactory.createMatteBorder(0, 0, 0, 0, Color.GREEN));
             vehiclePanelArray.get(pos2).setBorder(BorderFactory.createMatteBorder(0, 0, 0, 0, Color.GREEN));
@@ -299,11 +296,14 @@ public class VehicleGrid extends javax.swing.JPanel implements ActionListener, K
                     vehiclePanelArray.get(indicator).removeMouseListener(this);
                     panelHolder.remove(indicator);
                     vehiclePanelArray.remove(indicator);
-                    vehiclePanelClickedFlags.remove(indicator);
+                    vehiclePanelClickedFlags.get(indicator).setTruthValue(false);
+                    
                     --vehicleCount;
                     --trueCount;
-                    --expNumberOfVehicles;
+                    //--expNumberOfVehicles;
                     
+                    panelHolder.add(new JPanel());
+                    panelHolder.get(15).setBackground((new java.awt.Color(255, 255, 255)));
                     
                     reDraw();
                     
@@ -323,6 +323,7 @@ public class VehicleGrid extends javax.swing.JPanel implements ActionListener, K
     private void reDraw() {
         
         this.removeAll();
+        gbc = new GridBagConstraints();
         
         for(int m = 0; m < panelHolder.size(); m++) {
             if (m >= 8) {
@@ -354,12 +355,13 @@ public class VehicleGrid extends javax.swing.JPanel implements ActionListener, K
         addVehiclePanel(vp);
 
         columnCount++;
-            
+         
+        /*
         if (columnCount == 8) {
             rowCount = 1;
             columnCount = 0;
             
-        }
+        } */
         
         revalidate();
         repaint();
@@ -371,7 +373,7 @@ public class VehicleGrid extends javax.swing.JPanel implements ActionListener, K
          // handles vehiclePanelArray elements
         try {
             for (int i = 0; i < vehiclePanelArray.size() ; i++) {
-                if (e.getSource() == vehiclePanelArray.get(i)) {
+                if (e.getSource() == vehiclePanelArray.get(i)) {  
                     setFlag(i);
                     break;
                 }
